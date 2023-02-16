06c7df0563686795eb27275af3f55a248191b57e
Candice Jovon Juarez <candice.artist@gmail.com>
2023-02-16 09:11:57 -0500

    new wireframe added functions

---
+++ app.js
@@ -0,0 +1,20 @@
+const obiWon = ()=>{
+    let bioWon = document.querySelector(".displayText1")
+    bioWon.innerHTML = "A legendary Jedi Master, Obi-Wan Kenobi was a noble man and gifted in the ways of the Force. He trained Anakin Skywalker, served as a general in the Republic Army during the Clone Wars, and guided Luke Skywalker as a mentor."
+}
+const lukeSky = ()=>{
+    let bioLuke = document.querySelector(".displayText1")
+    bioLuke.innerHTML = "Luke Skywalker was a Tatooine farmboy who rose from humble beginnings to become one of the greatest Jedi the galaxy has ever known. Along with his friends Princess Leia and Han Solo, Luke battled the evil Empire, discovered the truth of his parentage, and ended the tyranny of the Sith."
+}
+const leiaSky = ()=>{
+    let bioLeia = document.querySelector(".displayText1")
+    bioLeia.innerHTML = "Princess Leia Organa was one of the greatest leaders of the Rebel Alliance, fearless on the battlefield and dedicated to ending the Empire's tyranny. Daughter of Padmé Amidala and Anakin Skywalker, sister of Luke Skywalker, and with a soft spot for scoundrels, Leia ranked among the galaxy's great heroes."
+}
+const aniSky = ()=>{
+    let bioAnakin = document.querySelector(".displayText1")
+    bioAnakin.innerHTML = "Anakin Skywalker was a Human male born on the Outer Rim world of Tatooine. He later served the Galactic Republic as a Jedi Knight, and later the Galactic Empire as the Sith Lord Darth Vader. Skywalker was believed to be the one known as 'The Chosen One' of Jedi prophecy, destined to bring balance to the Force."
+}
+const quiGon = ()=>{
+    let bioJinn = document.querySelector(".displayText1")
+    bioJinn.innerHTML = "A venerable if maverick Jedi Master, Qui-Gon Jinn was a student of the living Force. Qui-Gon lived for the moment, espousing a philosophy of 'feel, don't think -- use your instincts.' On Tatooine, Qui-Gon discovered a young slave boy named Anakin Skywalker who was strong in the Force."
+}
